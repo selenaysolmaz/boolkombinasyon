3861681422cc06104e71b2ed0da49264993622bc
semvs <semvs@ayvansaray.edu.tr>
2018-12-12 13:55:06 +0300

    Add project files.

---
+++ bom1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kac adet sayı için tüm T,F kombinasyolarını görmek istersiniz?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            BoolKom(n);
+            Console.ReadKey();
+        }
+        static void BoolKom(int n)
+        {
+            BoolKomYrd(" ", n);
+        }
+        static void BoolKomYrd(string oncekiler, int n)
+        {
+            if (n > 0)
+            {
+                BoolKomYrd(oncekiler + "true,", (n - 1));
+                BoolKomYrd(oncekiler + "false,", (n - 1));
+            }
+            else
+            {
+                Console.WriteLine(oncekiler);
+            }
+        }
+    }
+}
